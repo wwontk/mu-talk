4d73b769a2cc8d26dc4a480474625982a5139176
wwontk <wwontk@gmail.com>
2024-01-15 16:52:23 +0900

    게시판, 글쓰기 페이지 라우터 추가

--- src/PageNavigator.jsx
+++ src/PageNavigator.jsx
@@ -3,6 +3,8 @@ import PageLogin from "./Layout/PageLogin";
 import PageSignUp from "./Layout/PageSignUp";
 import PageMain from "./Layout/PageMain";
 import PageMyInfo from "./Layout/PageMyInfo";
+import PageBoard from "./Layout/PageBoard";
+import PagePosting from "./Layout/PagePosting";
 
 const PageNavigator = () => {
   return (
@@ -11,6 +13,8 @@ const PageNavigator = () => {
       <Route path="/login" element={<PageLogin />} />
       <Route path="/signup" element={<PageSignUp />} />
       <Route path="/mypage" element={<PageMyInfo />} />
+      <Route path="/board/:name" element={<PageBoard />}></Route>
+      <Route path="/board/:name/posting" element={<PagePosting />}></Route>
     </Routes>
   );
 };
