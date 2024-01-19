21e23b83ffbc31c22633ea17d3e7b1b1d4ea5032
wwontk <wwontk@gmail.com>
2024-01-19 21:31:42 +0900

    개별 포스팅 라우터 설정

--- src/PageNavigator.jsx
+++ src/PageNavigator.jsx
@@ -5,6 +5,9 @@ import PageMain from "./Layout/PageMain";
 import PageMyInfo from "./Layout/PageMyInfo";
 import PageBoard from "./Layout/PageBoard";
 import PagePosting from "./Layout/PagePosting";
+import PageShowPost from "./Layout/PageShowPost";
+import PagePostUpdate from "./Layout/PagePostUpdate";
+import PageList from "./Layout/PageList";
 
 const PageNavigator = () => {
   return (
@@ -15,6 +18,12 @@ const PageNavigator = () => {
       <Route path="/mypage" element={<PageMyInfo />} />
       <Route path="/board/:name" element={<PageBoard />}></Route>
       <Route path="/board/:name/posting" element={<PagePosting />}></Route>
+      <Route path="/board/:name/:postno" element={<PageShowPost />}></Route>
+      <Route
+        path="/board/:name/:postno/update"
+        element={<PagePostUpdate />}
+      ></Route>
+      <Route path="/boardlist" element={<PageList />}></Route>
     </Routes>
   );
 };
