4b0ccc812a75afb31cb2c6f711a819efc2184b77
wwontk <wwontk@gmail.com>
2024-01-19 21:35:12 +0900

    댓글 등록 컴포넌트

---
+++ src/Component/CommentCreate.jsx
@@ -0,0 +1,73 @@
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { auth, db } from "../firebase";
+import { addDoc, collection } from "firebase/firestore";
+
+const CommentCreate = () => {
+  const { name, postno } = useParams();
+  const [comment, setComment] = useState("");
+  const [userData, setUserData] = useState([]);
+
+  const commentPath = `boards/${name}/post/${postno}/comment`;
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserData(user);
+      }
+    });
+  }, []);
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await addDoc(collection(db, commentPath), {
+      text: comment,
+      userid: userData.uid,
+      writer: userData.displayName,
+      date: new Date(),
+    });
+    setComment("");
+  };
+  return (
+    <>
+      <CommentBox onSubmit={handleSubmit} autoComplete="false">
+        <CommentTxtArea
+          name="comment"
+          type="text"
+          value={comment}
+          onChange={handleChange}
+          autoComplete="false"
+        ></CommentTxtArea>
+        <CommentButton>댓글 등록</CommentButton>
+      </CommentBox>
+    </>
+  );
+};
+
+const CommentBox = styled.form`
+  display: flex;
+`;
+
+const CommentTxtArea = styled.input`
+  flex: 1;
+  border: 1px solid #c0c0c0;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const CommentButton = styled.button`
+  border: none;
+  padding: 0.3rem 1rem;
+  margin-left: 1rem;
+  background-color: #000;
+  color: #fff;
+
+  cursor: pointer;
+`;
+
+export default CommentCreate;
