6692a320aa58a6cb2ae42dba709dcb03b86b089c
wwontk <wwontk@gmail.com>
2024-01-19 21:38:52 +0900

    width 800px으로 고정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import { BrowserRouter } from "react-router-dom";
 import PageNav from "./Common/PageNav";
 import PageTitle from "./Common/PageTitle";
 import PageNavigator from "./PageNavigator";
+import styled from "@emotion/styled";
 
 function App() {
   return (
     <BrowserRouter>
-      <PageNav />
-      <PageTitle />
-      <PageNavigator />
+      <Body>
+        <PageNav />
+        <PageTitle />
+        <PageNavigator />
+      </Body>
     </BrowserRouter>
   );
 }
+
+const Body = styled.div`
+  width: 800px;
+  margin: 0 auto 2rem;
+`;
 
 export default App;
