cc6e3fe032a3f21e88a58b9a833662f5929abd33
wwontk <wwontk@gmail.com>
2024-01-25 00:16:39 +0900

    에디터 업데이트, 공지 여부도 함께 수정 가능

--- src/Layout/PagePostUpdate.jsx
+++ src/Layout/PagePostUpdate.jsx
@@ -1,26 +1,71 @@
 import styled from "@emotion/styled";
-import { doc, updateDoc } from "firebase/firestore";
-import { useState } from "react";
+import {
+  Timestamp,
+  collection,
+  doc,
+  onSnapshot,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { db } from "../firebase";
+import { Editor } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
 
 const PagePostUpdate = () => {
   const location = useLocation();
   const data = location.state.postData;
-  const { text, title, isnotice } = data;
+  const { text, title, isnotice, date, userid } = data;
   const [editdata, setEditdata] = useState({
     text,
     title,
+    isnotice,
   });
+  const [allPostPath, setAllPostPath] = useState("");
   const { name, postno } = useParams();
   const postPath = `boards/${name}/post/${postno}`;
   const navigate = useNavigate();
+
+  const editorRef = useRef();
+
+  useEffect(() => {
+    if (date && userid) {
+      const timestamp = Timestamp.fromDate(
+        new Date(date.seconds * 1000 + date.nanoseconds / 1000000)
+      );
+      const allquery = query(
+        collection(db, "AllPost"),
+        where("userid", "==", userid),
+        where("date", "==", timestamp)
+      );
+      onSnapshot(allquery, (snapshot) => {
+        const allArray = snapshot.docs.map((doc) => doc.id);
+        setAllPostPath(`AllPost/${allArray}`);
+      });
+    }
+  }, [date, userid]);
 
   const handleChange = (e) => {
     const { value, name } = e.target;
     setEditdata({
       ...editdata,
       [name]: value,
+    });
+  };
+
+  const handleToastChange = () => {
+    setEditdata({
+      ...editdata,
+      text: editorRef.current?.getInstance().getHTML(),
+    });
+  };
+
+  const handleCheckChange = () => {
+    setEditdata({
+      ...editdata,
+      isnotice: !editdata.isnotice,
     });
   };
 
@@ -35,29 +80,50 @@ const PagePostUpdate = () => {
     const editInfo = {
       text: editdata.text,
       title: editdata.title,
+      isnotice: editdata.isnotice,
+    };
+    const editInfoAll = {
+      title: editdata.title,
+      isnotice: editdata.isnotice,
     };
     const ref = doc(db, postPath);
+    const allref = doc(db, allPostPath);
     await updateDoc(ref, editInfo);
+    await updateDoc(allref, editInfoAll);
     alert("수정되었습니다.");
     navigate(`/board/${name}/${postno}`, {
-      state: { isnotice },
+      state: { isnotice: editdata.isnotice },
     });
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
+        <NoticeWrap>
+          <label for="notice">✔️공지</label>
+          <input
+            type="checkbox"
+            name="notice"
+            id="notice"
+            onChange={handleCheckChange}
+            defaultChecked={editdata.isnotice}
+          />
+        </NoticeWrap>
         <TitleInput
           name="title"
           type="text"
           value={editdata.title}
           onChange={handleChange}
         />
-        <TextareaInput
-          name="text"
-          type="text"
-          value={editdata.text}
-          onChange={handleChange}
+        <Editor
+          ref={editorRef}
+          initialValue={editdata.text}
+          previewStyle="vertical"
+          height="400px"
+          initialEditType="wysiwyg"
+          useCommandShortcut={false}
+          hideModeSwitch={true}
+          onChange={handleToastChange}
         />
         <Button>수정</Button>
         <Button type="button" onClick={handleCancelButton}>
@@ -67,6 +133,10 @@ const PagePostUpdate = () => {
     </>
   );
 };
+
+const NoticeWrap = styled.div`
+  margin-bottom: 1rem;
+`;
 
 const TitleInput = styled.input`
   box-sizing: border-box;
@@ -79,23 +149,24 @@ const TitleInput = styled.input`
   }
 `;
 
-const TextareaInput = styled.textarea`
-  box-sizing: border-box;
-  width: 100%;
-  height: 300px;
-  padding: 0.5rem;
-  border: 1px solid #c0c0c0;
-  margin-bottom: 1rem;
-  &:focus {
-    outline: none;
-  }
-`;
+// const TextareaInput = styled.textarea`
+//   box-sizing: border-box;
+//   width: 100%;
+//   height: 300px;
+//   padding: 0.5rem;
+//   border: 1px solid #c0c0c0;
+//   margin-bottom: 1rem;
+//   &:focus {
+//     outline: none;
+//   }
+// `;
 
 const Button = styled.button`
   border: 1px solid #c0c0c0;
   background-color: white;
-  padding: 0.5rem 1rem;
+  margin-top: 1rem;
   margin-right: 0.5rem;
+  padding: 0.5rem 1rem;
   cursor: pointer;
 `;
 
