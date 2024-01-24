780b70b2dee6016e442f12e1698fa459d047a1d7
wwontk <wwontk@gmail.com>
2024-01-25 02:45:13 +0900

    스토리지 추가

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,10 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+import firebase from "firebase/compat/app";
 import { getFirestore } from "firebase/firestore";
+import "firebase/compat/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,10 +22,14 @@ const firebaseConfig = {
   appId: "1:137159898465:web:925ecb4f50603c5c7a8120",
 };
 
+firebase.initializeApp(firebaseConfig);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+export const storageService = firebase.storage();
 
 export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword };
